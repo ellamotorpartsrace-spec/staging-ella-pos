bf4415be9b80403fd84266658f8ff4bef5145810
Les <lester@gmail.com>
2026-06-02 11:25:45 +0800

    clean up debug scripts

--- ella-pos/query2.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config/config.php';
-require_once 'config/database.php';
-$db = new Database();
-$conn = $db->getConnection();
-$sql = "SELECT * FROM inventory WHERE variation_id = 399";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
