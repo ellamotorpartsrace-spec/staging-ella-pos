db7cf8e2230b51fc5be649201b5f1aa88538aa6e
Les <lester@gmail.com>
2026-06-09 09:01:25 +0800

    fix

---
+++ test_archived.php
@@ -0,0 +1,1 @@
+<?php session_start(); ['user_id']=1; ['role']='admin'; ['ip_address']='127.0.0.1'; ['REMOTE_ADDR']='127.0.0.1'; require 'c:\xampp\htdocs\ella-pos\ella-pos\views\inventory\archived.php';
